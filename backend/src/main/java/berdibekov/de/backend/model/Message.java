3879c722d4f6a644cf1682596c8ddf3afed7280a
daurenberdibekov <berder08@gmail.com>
2024-07-20 13:09:47 +0200

    Add message model and DTO

---
+++ backend/src/main/java/berdibekov/de/backend/model/Message.java
@@ -0,0 +1,39 @@
+package berdibekov.de.backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity(name = "message")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private String id;
+
+    @NotNull
+    private String subject;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
+}
