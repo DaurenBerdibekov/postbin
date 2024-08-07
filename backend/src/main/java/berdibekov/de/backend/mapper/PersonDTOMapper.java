efda2254168544acd2d8ab3307f52d08bde82791
daurenberdibekov <berder08@gmail.com>
2024-08-07 22:03:20 +0200

    Add repository and mapper

--- backend/src/main/java/berdibekov/de/backend/mapper/PersonDTOMapper.java
+++ backend/src/main/java/berdibekov/de/backend/mapper/PersonDTOMapper.java
@@ -4,11 +4,15 @@ import berdibekov.de.backend.dto.PersonDTO;
 import berdibekov.de.backend.model.Person;
 import org.mapstruct.Mapper;
 
+import java.util.Set;
+
 @Mapper(componentModel = "spring")
 public interface PersonDTOMapper {
 
     PersonDTO toDTO(Person person, String personId);
 
     Person toModel(PersonDTO personDTO);
+
+    Set<PersonDTO> toDTOs(Set<Person> personSet);
 
 }
--- backend/src/main/java/berdibekov/de/backend/repository/PersonRepository.java
+++ backend/src/main/java/berdibekov/de/backend/repository/PersonRepository.java
@@ -2,9 +2,12 @@ package berdibekov.de.backend.repository;
 
 import berdibekov.de.backend.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
@@ -12,4 +15,11 @@ public interface PersonRepository extends JpaRepository<Person, UUID> {
     Optional<Person> findById(String personId);
 
     Optional<Person> findByEmailAndPassword(String email, String password);
+
+    Optional<Person> findByFirstnameContainingIgnoreCaseAndLastnameContainingIgnoreCase(String firstname, String lastname);
+
+    @Query(value = "SELECT p.* FROM person p " +
+            "JOIN person_friends pf ON p.id = pf.friends_id " +
+            "WHERE pf.person_id = :personId", nativeQuery = true)
+    Set<Person> findFriendsByPersonId(@Param("personId") String personId);
 }
